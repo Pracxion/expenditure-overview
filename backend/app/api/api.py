50215b111a9fb60bcc365ab29362828f440c32b3
Alexander Kipka <kipka.alexander@gmail.com>
2025-04-04 22:06:26 +0200

    deleted unneccessary stuff

--- backend/app/api/api.py
+++
@@ -1,9 +0,0 @@
-
-from fastapi.routing import APIRouter
-
-api = APIRouter()
-
-
-@api.get('/', response_model=dict[str, str])
-async def hello_world():
-    return {"Hellohello": "World"}
--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-
-  return (
-    <div className="flex flex-col items-center justify-center min-w-[100vw] min-h-[100vh]">
-      <div>Header</div>
-    </div>
-  )
-}
-
